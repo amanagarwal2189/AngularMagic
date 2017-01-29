ab0d3b81eb534e06a50536436622cb634903a3f6
Aman Agarwal <aman.agarwal2189@gmail.com>
2017-01-28 19:10:32 -0600

    Add another component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
 moduleId:module.id,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+/**  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']*/
+  template: `
+  <h1>Inline Template</h1>
+  <p> Another Line</p>
+  `,
+  styles:[`
+  	h1{
+  		color: red;
+  	}
+  `]
 })
 export class AppComponent {
   title = 'The app is up and running!';
